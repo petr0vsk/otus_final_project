29fdd8e8c5263fb5daff6318463474d21d2f6257
petr0vsk <petrovskjy.aleksander@gmail.com>
2021-05-05 17:54:22 +0300

    Check form validyted

--- author/views.py
+++ author/views.py
@@ -8,5 +8,7 @@ author_app = Blueprint('author_app', __name__)
 @author_app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+    if form.validate_on_submit():
+        return "Ok, all validayted"
     return render_template('author/register.html', form = form)
     
