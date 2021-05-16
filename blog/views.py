a4a4c695764bedfb54ee6e8db0783c590940bb57
petr0vsk <petrovskjy.aleksander@gmail.com>
2021-05-16 16:52:14 +0300

    add view by category of posts

--- blog/views.py
+++ blog/views.py
@@ -21,7 +21,7 @@ def index():
     page = int(request.values.get('page', '1'))
     posts = Post.query.filter_by(live=True).order_by(Post.publish_date.desc())\
         .paginate(page, POSTS_PER_PAGE, False) # выведем посты в порядке поступления c пагинатором
-    return render_template('blog/index.html', posts=posts )
+    return render_template('blog/index.html', posts=posts, title="Последнее сообщение" )
 
 
 @blog_app.route('/post', methods=['GET', 'POST'])
@@ -135,6 +135,32 @@ def delete(slug):
     db.session.commit()
     flash("Article deleted")
     return redirect(url_for('.index'))
+
+@blog_app.route('/categories/<category_id>') # вывод по категории
+def categories(category_id):
+    category = Category.query.filter_by(id=category_id).first_or_404()
+    page = int(request.values.get('page', '1'))
+    posts = Post.query.filter_by(category=category, live=True)\
+        .order_by(Post.publish_date.desc())\
+        .paginate(page, POSTS_PER_PAGE, False)
+    return render_template('blog/category_posts.html',
+        posts=posts,
+        title=category,
+        category_id=category_id
+    )    
+
+@blog_app.route('/tags/<tag>') # вывод по тегу
+def tags(tag):
+    tag = Tag.query.filter_by(name=tag).first_or_404()
+    page = int(request.values.get('page', '1'))
+    posts = tag.posts.filter_by(live=True)\
+        .order_by(Post.publish_date.desc())\
+        .paginate(page, POSTS_PER_PAGE, False)
+    return render_template('blog/tag_posts.html',
+        posts=posts,
+        title="Tag: " + str(tag),
+        tag=str(tag)
+    )    
 
 # -------------------------------------------------------
 # сожмем картинку
